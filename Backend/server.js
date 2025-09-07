b8382ba15cf8dd7f1498547f184c08a50beb86dd
Kalyani Jadhav <kalyanijadhav114@gmail.com>
2025-09-08 01:48:00 +0530

    change4

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const PORT = 8080;
 app.use(express.json());
 app.use(cors());
 
-app.use("/*", chatRoutes);
+app.use("*", chatRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
